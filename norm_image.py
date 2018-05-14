e6132a21527afa5a2d0b0913ffa30836d28cfbd5
Gordon Tsai <gtsai13@gmail.com>
2018-05-13 20:03:45 -0500

    added changes to dim for most recent car price dist, added parsing 100 boxes correctly into one giant list in order

--- norm_image.py
+++ norm_image.py
@@ -21,7 +21,6 @@ pixels=im.load()
 # pixels[813,37]=(255,0,0)
 # # Lower right corner of graph
 # pixels[813,733]=(255,0,0)
-# im.show()
 
 
 # Setting Points
@@ -42,19 +41,61 @@ p_right_upper=(right_bnd,upper_bnd)
 num_boxes=10
 xstep=int(round(len_left_right/num_boxes,0))
 ystep=int(round(len_lower_upper/num_boxes,0))
+xextra=xstep-(len_left_right%xstep)
+yextra=ystep-(len_lower_upper%ystep)
 orig_box=(left_bnd,upper_bnd,right_bnd,lower_bnd)
-print('orig_box',orig_box)
 # Bottom left most 10 x 10 box
 # box=(left_bnd,lower_bnd-ystep*1,left_bnd+xstep*1,lower_bnd)
 # Upper left most 10 x 10 box
-box=(left_bnd,upper_bnd,left_bnd+xstep*1,upper_bnd+xstep*1)
-print('box',box)
-region=im.crop(box)
-region.show()
 
-print(dim)
+#store the ten boxes
+box_num=1
+for y in range(10):
+  box_list=[]
+  for x in range(10):
+
+    if x==9 and y==9:
+      left=left_bnd+xstep*(x)
+      up=upper_bnd+ystep*(y)
+      right=left_bnd+xstep*(x+1)-xextra
+      down=upper_bnd+ystep*(y+1)-yextra
+    elif x ==9:
+      left=left_bnd+xstep*(x)
+      up=upper_bnd+ystep*(y)
+      right=left_bnd+xstep*(x+1)-xextra
+      down=upper_bnd+ystep*(y+1)
+    elif y==9:
+      left=left_bnd+xstep*(x)
+      up=upper_bnd+ystep*(y)
+      right=left_bnd+xstep*(x+1)
+      down=upper_bnd+ystep*(y+1)-yextra
+    else:
+      left=left_bnd+xstep*(x)
+      up=upper_bnd+ystep*(y)
+      right=left_bnd+xstep*(x+1)
+      down=upper_bnd+ystep*(y+1)
+
+    box=(left,up,right,down)
+    # print('box ',str(box_num),box)
+    region=im.crop(box)
+    pixels[left,up]=(0,0,0)
+    pixels[left,down]=(0,0,0)
+    pixels[right,up]=(0,0,0)
+    pixels[right,down]=(0,0,0)
+
+    #assign to list in order
+    box_list.append(region)
+    #iterate
+    box_num+=1
+
+im.show()
+
+#display boxes
+# for img in box_list:
+  # img.show()
+
+
 print(sum(dim[0]))
-
 # box = (100, 100, 400, 400)
 # region = im.crop(box)
 
