a9e8820156c9d7295a82ee5cbf742be0d241e721
Gordon Tsai <gtsai13@gmail.com>
2018-08-08 22:00:28 -0500

    started to add defaultVariables which are imported into the model to use as defaults when nothing is specified

---
+++ defaultVariables.py
@@ -0,0 +1,19 @@
+
+
+
+Loan_Terms=48       # as months
+
+AIR=.03             # Annual Interest Rate as %
+MIR=AIR/12          # Monthly Interest Rate as %
+
+Car_Price=24000     # Car Price as $
+Down_Payment=5000   # as $
+
+
+#TNC Variables
+Fare_Base=2         # as $
+Fare_Per_Mile=1.5   # as $/mile
+Fare_Per_Minute=.20 # as $/minute
+
+
+Annual_Miles_Avg=13000 # as # of miles
---
+++ model.py
@@ -0,0 +1,232 @@
+from defaultVariables import *
+
+def Amortization(mnth = 12,loanterms= Loan_Terms, ir = MIR,carprice= Car_Price, downpmnt = Down_Payment):
+    loan = carprice-downpmnt #as $
+    pmnt = (ir*loan*(1+ir)**loanterms)/(((1+ir)**loanterms)-1)
+
+    #Test
+    #print(pmnt)
+    #print(ir)
+    #print(loan)
+    #print(Loan_Terms)
+    #print(mnth)
+
+    #Year1 Values
+    Interest_1 = loan*ir
+    #print(Interest_1)
+    Principal_1 = pmnt-Interest_1
+    #print(Principal_1)
+    Balance_1 = loan-Principal_1
+    Interest = ir*Balance_1
+    Balance = Balance_1
+    Total_Principal = Principal_1
+    Total_Interest = Interest_1
+    Total_Pmnt = pmnt*mnth
+    #print(Total_Pmnt)
+    for i in range(0,int(mnth-1)):
+        Interest = ir*Balance
+        Principal = pmnt-Interest
+        Balance = Balance-Principal
+        Total_Principal = Principal+Total_Principal
+        Total_Interest = Interest +Total_Interest
+    if mnth==1:
+        return pmnt, pmnt, Interest_1, Interest_1, Principal_1, Principal_1, Balance_1
+    else:
+        return pmnt,Total_Pmnt,Interest,Total_Interest,Principal,Total_Principal,Balance
+
+
+
+#####################################################################################################################
+#annualroi is set here
+
+def Uber_Expense_Model(outputyears = (7,10,15,20), annualroi = 5,
+                       farebase = Fare_Base, farepermile = Fare_Per_Mile, farepermin = Fare_Per_Minute,
+                       annualmiles = Annual_Miles_Avg, triptime = Trip_Time_Avg, numtrips = Num_Trips_Avg,
+                       timeworth= Time_Worth, waittime = Wait_Time_Uber_Avg, carprice = Car_Price):
+    #Calc Val_Time_Annual_Uber
+    Val_Time_Daily_Uber = (waittime*numtrips)/60*timeworth
+    valuetime = Val_Time_Daily_Uber*365
+
+    #Calc Trip_Dist_Avg
+    Daily_Miles_Avg = annualmiles/365   # as miles/day
+    Trip_Dist_Avg = Daily_Miles_Avg/numtrips
+
+    #Calc Uber_Exp_Annual
+    Uber_Trip_Avg = farebase+farepermile*Trip_Dist_Avg+farepermin*triptime
+    Uber_Exp_Daily =  Uber_Trip_Avg*Num_Trips_Avg
+    annualexpense = Uber_Exp_Daily*365
+
+    a,OUP_Expense_Car,Cum_Expense_Car = Car_Ownsership_Expense_Model(annualmiles=annualmiles,timeworth=timeworth, carprice= carprice)
+    #Convert annualroi to float
+    annualroi = float(annualroi)
+
+    array = np.zeros((10,len(OUP_Expense_Car)))
+    #Annual Fee to TNC
+    array[0] = annualexpense
+    #Value of Time Expense
+    array[1] = valuetime
+    #Total OUP Expense
+    array[2] = array[0] + array[1]
+    #Cum. OOP Expense
+    for i in range(0,len(OUP_Expense_Car)):
+        if i==0:
+            array[3,i]=array[2,i]
+        else:
+            array[3,i]=array[3,i-1]+array[2,i]
+    #Unrealized Annual Cash Savings
+    array[4] = array[2] - OUP_Expense_Car
+    #Cum Unrealized Cash Savings
+    for i in range(0,len(OUP_Expense_Car)):
+        if i==0:
+            array[5,i]=array[4,i]
+        else:
+            array[5,i]=array[5,i-1]+array[4,i]
+    ##Unrealized Value of Invested Savings
+    for i in range(0,len(OUP_Expense_Car)):
+        if i==0:
+            array[6,i]=array[4,i]
+        else:
+            array[6,i]=array[4,i]+(array[6,i-1]*(1+annualroi/100))
+    #Unrealized Added Value of Investing
+    array[7] = array[6]-array[5]
+
+    #Total Expense for Using Uber
+    array[8] = array[3]+array[7]
+
+    #Expense Delta
+    array[9] = Cum_Expense_Car-array[8]
+
+    #output array
+    output = np.zeros((1,len(outputyears)))
+    for i in range(0,len(outputyears)):
+        output[0,i] = array[8,outputyears[i]-1]
+    return output
+
+#Test
+#a = Uber_Expense_Model(timeworth=Time_Worth[0],annualmiles=Annual_Miles_Avg[0],carprice = Car_Price[0])
+#print(a)
+###################################################################################################################
+
+
+
+#####################################################################################################################
+def Car_Ownsership_Expense_Model(outputyears = (7,10,15,20), n = [7,10,15,20], purchasefees = Purchase_Fees,
+                                 downpmnt = Down_Payment,ide = Total_IDE, timeworth= Time_Worth,
+                                 annualmiles = Annual_Miles_Avg, loanterms = Loan_Terms,
+     years= Model_Length,carprice = Car_Price, milespergallon = MPG, triptime = Trip_Time_Avg,
+         fuelprice = Fuel_Price, numtrips = Num_Trips_Avg, taxpercent = Sales_Tax_Percent,
+         maintenance = Maint_Repairs_Exp, insurance = Insurance_Exp,registrationtaxes = Registr_Taxes_Exp,
+        parking = Parking_Exp):
+
+
+    Fuel_Exp = annualmiles/milespergallon*fuelprice
+    de = maintenance+insurance+registrationtaxes+parking+Fuel_Exp
+
+
+    salestax = carprice*taxpercent #as $
+    Loan_Amount = carprice - Down_Payment #as $
+
+
+    Daily_Miles_Avg = annualmiles/365   # as miles/day
+    Trip_Dist_Avg = Daily_Miles_Avg/numtrips
+    Drive_Time_Avg = triptime*numtrips   # as minutes
+
+    #timeworth is in here
+    Val_Drive_Time_Daily = timeworth*Drive_Time_Avg/60
+    Val_Walk_Time_Daily = Walk_Time_Avg/60*timeworth
+    Val_Time_Daily_Car =Val_Drive_Time_Daily+Val_Walk_Time_Daily
+    Val_Time_Annual_Car = (Val_Time_Daily_Car)*365
+    valuetime = Val_Time_Annual_Car
+
+
+    loanyears = loanterms/12
+    array = np.zeros((13,years))
+    [rows,columns] = array.shape
+    [a,b,c,d,e,f,g] = Amortization(mnth=12,carprice=carprice)
+    Principal_Incr = f
+    Interest_Incr = d
+
+    #Test
+    #print(a,b,c,de,f,g)
+    #print(Principal_Incr)
+    #print(Interest_Incr)
+
+    #assigning Cost at Time of Purchase
+    cost = salestax+purchasefees+downpmnt
+    array[0,0] = cost
+    #Principal Paid and Interest Paid
+    for i in range(0,columns):
+        if i > loanyears-1:
+            array[1,i] = 0.0
+            array[2,i] = 0.0
+        elif i == 0:
+            array[1,i] = Principal_Incr
+            array[2,i] = Interest_Incr
+        else:
+            [a,b,c,d,e,f,g] = Amortization(mnth=(i+1)*12,carprice=carprice)
+            [h,j,k,l,m,n,o] = Amortization(mnth = i*12,carprice=carprice)
+            Principal_Incr = f-n
+            Interest_Incr = d-l
+            array[1,i] = Principal_Incr
+            array[2,i] = Interest_Incr
+
+    #Assigning DE and IDE
+    array[3] = de
+    array[4] = ide
+
+    #Summing expense Total Value Out of Pocket Expense and Cumulative Out of Pocket Expense
+    array[5] = array[0]+array[1]+array[2]+array[3]+array[4]
+    for i in range(0,columns):
+        if i==0:
+            array[6,i]=array[5,i]
+        else:
+            array[6,i]=array[5,i]+array[6,i-1]
+
+    #Annual Value of Time and Cumulative Value of Time
+    array[7] = valuetime
+    for i in range(0,columns):
+        if i==0:
+            array[8,i]=array[7,i]
+        else:
+            array[8,i]=array[7,i]+array[8,i-1]
+
+    #Total Annual Expense for Owning and Cum. Expense for Owning
+    array[9] = array[7] +array[5]
+    for i in range(0,columns):
+        if i==0:
+            array[10,i]=array[9,i]
+        else:
+            array[10,i]=array[9,i]+array[10,i-1]
+
+    #Value of Car
+    array[11] = Depreciation(20,4)*carprice
+
+    #Cumulative Expenses Less value of Car
+    array[12] = array[10] - array[11]
+
+    output = np.zeros((2,len(outputyears)))
+    for i in range(0,len(outputyears)):
+        output[0,i] = array[10,outputyears[i]-1]
+        output[1,i] = array[12,outputyears[i]-1]
+
+
+    return output,array[5],array[10]
+
+
+
+#####################################################################################################################
+def Depreciation(initialrate,rate,years = Model_Length):
+    years = int(math.ceil(years))
+    array = np.zeros((1,years))
+    initialrate = initialrate/100.0
+    rate = rate/100.0
+    totalpercent = 1.0
+    for i in range(0,(array.size)):
+        if initialrate>.1:
+            array[0][i] = totalpercent*(1-initialrate)
+            initialrate = initialrate - rate
+            totalpercent= array[0][i]
+        else:
+            array[0][i] = totalpercent*(1-.1)
+            totalpercent= array[0][i]
+    return array
